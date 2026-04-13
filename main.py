74221f0547c3d0fd7696e034a6f2edc4dc9d36e0
Arthur Fernandes <118545996+Arthur-Fernandes-Barros@users.noreply.github.com>
2026-04-13 17:47:40 +0000

    Melhoras front-end

--- main.py
+++ main.py
@@ -5,24 +5,34 @@ def campoharmonico(tonica):
     notas = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
     if tonica not in notas or not isinstance(tonica, str):
-        # Substituído 'return' padrão por um aviso de erro do Streamlit
         st.error('Nota inválida')
     else:
         if tonica in notas:
             posicao = notas.index(tonica)
-            # Substituído 'print' por 'st.write' para exibir na interface web
-            st.write(
-                f'**Campo harmônico maior:** {notas[posicao]} {notas[ posicao - 10]}m {notas[ posicao - 8]}m {notas[posicao - 7]} {notas[ posicao - 5]} {notas[ posicao - 3]}m {notas[posicao - 1]}m7')
-            st.write(
-                f'**Campo harmônico menor:** {notas[posicao]}m {notas[ posicao - 10]} {notas[ posicao - 9]}m {notas[posicao - 7]} {notas[ posicao - 5]} {notas[ posicao - 4]}m {notas[posicao - 2]}')
-            st.write(
-                f'**Campo harmônico relativo menor:** {notas[(posicao-3)%12]}m {notas[((posicao-3)-10)%12]} {notas[((posicao-3)-9)%12]}m {notas[((posicao-3)-7)%12]} {notas[((posicao-3)-5)%12]} {notas[((posicao-3)-4)%12]}m {notas[((posicao-3)-2)%12]}')
+
+            # Strings formatadas com os intervalos (I, II, III, etc.)
+            maior = f"**I:** {notas[posicao]} | **II:** {notas[ posicao - 10]}m | **III:** {notas[ posicao - 8]}m | **IV:** {notas[posicao - 7]} | **V:** {notas[ posicao - 5]} | **VI:** {notas[ posicao - 3]}m | **VII:** {notas[posicao - 1]}m"
+
+            menor = f"**I:** {notas[posicao]}m | **II:** {notas[ posicao - 10]} | **III:** {notas[ posicao - 9]}m | **IV:** {notas[posicao - 7]} | **V:** {notas[ posicao - 5]} | **VI:** {notas[ posicao - 4]}m | **VII:** {notas[posicao - 2]}"
+
+            relativo = f"**I:** {notas[(posicao-3)%12]}m | **II:** {notas[((posicao-3)-10)%12]} | **III:** {notas[((posicao-3)-9)%12]}m | **IV:** {notas[((posicao-3)-7)%12]} | **V:** {notas[((posicao-3)-5)%12]} | **VI:** {notas[((posicao-3)-4)%12]}m | **VII:** {notas[((posicao-3)-2)%12]}"
+
+            # Exibição formatada no Streamlit
+            st.subheader("Campo Harmônico Maior")
+            st.info(maior)
+
+            st.subheader("Campo Harmônico Menor")
+            st.info(menor)
+
+            st.subheader("Campo Harmônico Relativo Menor")
+            st.info(relativo)
 
 # --- Interface em Streamlit ---
 
 
-st.title("🎵 Gerador de Campo Harmônico")
-st.write("Escolha a nota tônica para visualizar os seus respectivos campos harmônicos.")
+# Título alterado conforme solicitado
+st.title("🎵 Bragi_intervals")
+st.write("Escolha a nota tônica para visualizar os seus respectivos campos harmônicos e intervalos.")
 
 # Lista de notas para o menu de seleção
 notas_disponiveis = ['C', 'C#', 'D', 'D#',
